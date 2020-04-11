5e8b76b5df13ca53e4670ae0bcc670f9891a0b1a
Andrey Grishanov <provvx@gmail.com>
2020-04-12 00:47:52 +0300

    homework #1
    1. Добавлено 2 фильма с названиями и кнопками "Details"
    2. По нажатию на кнопку "Details" происходит переход на другое Активити
    3. При нажатии на "Details" название фильма меняет цвет на красный, при возвращении на MainActivity название фильма, "детали" которого только что были открыты, остается красным
    4. Информация о поле checkBox и notes при возврате на MainActivity выводится в лог под тэгом "MyTag"

--- app/src/main/java/com/example/otusproject/MainActivity.kt
+++ app/src/main/java/com/example/otusproject/MainActivity.kt
@@ -1,12 +1,62 @@
 package com.example.otusproject
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import com.example.otusproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.detailsBtn1.setOnClickListener{
+            binding.movieName1.setTextColor(Color.RED)
+            val intent = Intent(this, Movie1Activity::class.java)
+            startActivity(intent)
+        }
+
+        binding.detailsBtn2.setOnClickListener{
+            binding.movieName2.setTextColor(Color.RED)
+            val intent = Intent(this, Movie2Activity::class.java)
+            startActivity(intent)
+        }
+
+        binding.inviteBtn.setOnClickListener {
+            val intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.inviteMessage))
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(intent, null)
+            startActivity(shareIntent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val intent = intent
+        val name = intent.getStringExtra("movieName")
+        if (name != null) {
+            when (name) {
+            "Blade Runner 2049" -> binding.movieName1.setTextColor(Color.RED)
+            "Joker" -> binding.movieName2.setTextColor(Color.RED)
+            }
+
+            when (intent.getBooleanExtra("checkBox", false)) {
+                true -> Log.d("MyTag", "$name is your favorite")
+                false -> Log.d("MyTag", "$name is not your favorite")
+            }
+            Log.d("MyTag", "$name notes: ${intent.getStringExtra("notes")}")
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/otusproject/Movie1Activity.kt
@@ -0,0 +1,64 @@
+package com.example.otusproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.core.text.set
+import com.example.otusproject.databinding.ActivityMainBinding
+import com.example.otusproject.databinding.MovieDetails1LayoutBinding
+
+class Movie1Activity : AppCompatActivity() {
+    private lateinit var binding: MovieDetails1LayoutBinding
+    private var noteText = ""
+    private lateinit var note: Editable
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MovieDetails1LayoutBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.notes.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s != null) {
+                    note = s
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                noteText = s.toString()
+            }
+
+        })
+    }
+
+    override fun onBackPressed() {
+        val intentM1 = Intent(this@Movie1Activity, MainActivity::class.java)
+        intentM1.putExtra("checkBox", binding.checkBox.isChecked)
+        intentM1.putExtra("movieName", "Blade Runner 2049")
+        intentM1.putExtra("notes", noteText)
+        startActivityForResult(intentM1, 1)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString("NOTES_TEXT", noteText)
+        outState.putBoolean("isBox", binding.checkBox.isChecked)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        binding.notes.setText(savedInstanceState["NOTES_TEXT"].toString())
+        binding.checkBox.isChecked = savedInstanceState["isBox"] as Boolean
+    }
+}
